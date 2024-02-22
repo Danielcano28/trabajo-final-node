3a4de1505291662ab1135c14c8b4150d36b48207
daniel cano castrillon <canocastrillondaniel444@gmail.com>
2024-02-22 13:43:04 -0500

    advances

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,9 @@
+const Category = require("./Category");
+const Product = require("./Product");
 const User = require("./User");
+
+
+// product -> categoryId
+Product.belongsTo(Category)
+Category.hasMany(Product)
 
